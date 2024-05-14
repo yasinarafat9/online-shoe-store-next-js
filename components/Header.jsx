bf70c112dd4ed8ffeb178a8e316912d452f259a5
unknown <yasinarafat9612@gmail.com>
2024-05-14 15:05:15 +0600

    tailwind fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Wrapper from "./Wrapper";
+
 
 const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lastScrolly, setLastScrolly] = useState(0);
+
+
   return (
-    <div>
-      <div className="text-red-600">Header</div>
-    </div>
+
+    <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper>HFFF</Wrapper>
+    </header>
+
+
   );
 };
 
