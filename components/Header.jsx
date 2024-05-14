1bfbb7590bc86faea7d4b5f45fc9bee173554718
unknown <yasinarafat9612@gmail.com>
2024-05-14 15:15:11 +0600

    wrapper images pasted in public folder after downloading

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Wrapper from "./Wrapper";
+import Link from "next/link";
 
 
 const Header = () => {
@@ -13,7 +14,11 @@ const Header = () => {
 
     <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
     >
-      <Wrapper>HFFF</Wrapper>
+      <Wrapper>
+        <Link href="/">
+          <img src="/logo.svg" className="w-[40px] md:w-[60px]" alt="" />
+        </Link>
+      </Wrapper>
     </header>
 
 
--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Wrapper = ({ children, className }) => {
     return (
-        <div className={`w-full max-2-[1280px] px-5 md:px-10 mx-auto ${className || ""}`}>
+        <div className={`w-full max-w-[1280px] px-5 md:px-10 mx-auto ${className || ""}`}>
             {children}
         </div>
     );
