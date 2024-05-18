588c76e5163b285b543e27892cdd2d4241af1012
unknown <yasinarafat9612@gmail.com>
2024-05-18 21:48:47 +0600

    Heading and paragraph written

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
   return (
     <main className="">
       <HeroBanner></HeroBanner>
+      <Wrapper>
+        {/* Heading and Paragraph Start */}
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text=[34px] mb-5 font-semibold leading-tight">
+            Cushioning for Your Miles
+          </div>
+          <div className="text-md md:text-xl">
+            A Lightweight Nike ZoomX midsole is combined with increased stack heights to help provide cushioning during extended stretches of running.
+          </div>
+        </div>
+        {/* Heading and Paragraph End */}
+      </Wrapper>
     </main>
   );
 }
