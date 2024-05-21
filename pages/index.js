34d3d88fa2c7a7271fe4ad70567e071bc2593102
unknown <yasinarafat9612@gmail.com>
2024-05-22 00:11:11 +0600

    grid product card demo added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import HeroBanner from "@/components/HeroBanner";
+import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
@@ -16,6 +17,24 @@ export default function Home() {
           </div>
         </div>
         {/* Heading and Paragraph End */}
+
+        {/* Product Cards Grid Start */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+        {/* Product Cards Grid End */}
+
       </Wrapper>
     </main>
   );
