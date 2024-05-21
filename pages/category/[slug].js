f073a8a8636e5bb2c0cf6c35fad0e099c081d3d3
unknown <yasinarafat9612@gmail.com>
2024-05-22 01:22:13 +0600

    category page [slug].js created

---
+++ pages/category/[slug].js
@@ -0,0 +1,35 @@
+import Wrapper from '@/components/Wrapper';
+import React from 'react';
+
+const Category = () => {
+    return (
+        <div className='w-full md:py-20'>
+            <Wrapper>
+                <div className='text-center max-w-[800px] mx-auto mt-8 md:mt-0'>
+                    <div className='text-[28px] md:text-[34px] mb-5 font-semibold leading-tight'>
+                        Running Shoes
+                    </div>
+                </div>
+
+                {/* Product Cards Grid Start */}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
+                {/* Product Cards Grid End */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default Category;
