f0d5ab64b563031ce83a328aa43e743501e01f38
unknown <yasinarafat9612@gmail.com>
2024-05-23 21:30:37 +0600

    cart page done

--- pages/cart.js
+++ pages/cart.js
@@ -1,5 +1,7 @@
 import CartItem from '@/components/CartItem';
 import Wrapper from '@/components/Wrapper';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Cart = () => {
@@ -26,10 +28,59 @@ const Cart = () => {
                     {/* Summery Start */}
                     <div className='flex-[1]'>
                         <div className='text-lg font-bold'>Summary</div>
+
+                        <div className='p-5 my-5 bg-black/[0.05] rounded-xl'>
+                            <div className='flex justify-between'>
+                                <div className='uppercase text-md md:text-lg font-medium text-black'>
+                                    Subtotal
+                                </div>
+                                <div className='text-md md:text-lg font-medium text-black'>
+                                    Rs 19 695.00
+                                </div>
+                            </div>
+                            <div className='text-sm md:text-md py-5 border-t mt-5'>
+                                This user guide explains how a Sales Consultant can add price line items to a sales quote price table and then group price line items to summarise up to a sub-heading.
+                            </div>
+
+                            {/* Button Start */}
+                            <button className='w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75'>
+                                Checkout
+                            </button>
+                            {/* Button End */}
+                        </div>
                     </div>
                     {/* Summery End */}
                 </div>
                 {/* Cart Content End */}
+
+
+
+                {/* This is Empty Screen */}
+                {/* <div className='flex-[2] flex flex-col items-center pb-[50px] md:-mt-14'>
+                    <Image
+                        src="/empty-cart.jpg"
+                        width={300}
+                        height={300}
+                        className='w-[300px] md:w-[400px]'
+                    />
+                    <span className='text-xl font-bold'>
+                        Your Cart is Empty
+                    </span>
+                    <span className='text-center mt-4'>
+                        Looks like you have not added anything in your cart.
+                        <br />
+                        Go ahead and explore top categories.
+                    </span>
+
+                    <Link
+                        href='/'
+                        className='py-4 px-8 text-center  rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 mt-8'
+                    >
+                        Continue Shopping
+                    </Link>
+                </div> */}
+
+
             </Wrapper>
         </div>
     );
